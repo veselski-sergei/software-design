41d8472ff2977cfa326c1f483ab785b0c94016c2
KasperZ <40818603+zaytseffffffff@users.noreply.github.com>
2021-12-01 16:32:51 +0300

    Add relay skript

---
+++ relay_on.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#project: poly-smart-home.ru
+import RPi.GPIO as GPIO
+
+gpio_pin_number = 20  #на 20 пине (BCM) сигнальный контакт реле
+print("r_on use pin:" + str(gpio_pin_number))
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(gpio_pin_number, GPIO.OUT)
+GPIO.output(gpio_pin_number, GPIO.HIGH)
---
+++ relay_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#project: poly-smart-home.ru
+import subprocess
+from subprocess import Popen, PIPE
+
+pin_number = 20 #пин с сигнальным проводом реле
+
+proc = Popen(
+    "echo %s > /sys/class/gpio/export" % pin_number,
+    shell=True,
+    stdout=PIPE, stderr=PIPE
+)
+proc.wait()
+proc = Popen(
+    "cat /sys/class/gpio/gpio%s/value" % pin_number,
+    shell=True,
+    stdout=PIPE, stderr=PIPE
+)
+proc.wait()
+res = proc.communicate()
+count = res[0].replace("\n","")
+count = int(count)
+if count == 0:
+        print('0').replace("\n","")
+else:
+        print('1').replace("\n","")
