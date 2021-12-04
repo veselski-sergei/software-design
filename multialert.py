0d7ffcbc7d4a0ee23c57f18970f1fe6e86c57578
veselski-sergei <sveselski167@gmail.com>
2021-12-04 21:27:40 +0300

    Implement alert-class motion_check

--- multialert.py
+++ multialert.py
@@ -10,10 +10,12 @@ from threading import Thread
 
 t_s = 3
 t_s2 = 30
+t_s3 = 30
 
 file_path = os.getcwd()
 
 water_id = file_path + '/alert_state/w_on'
+motion_id = file_path + '/alert_state/m_on'
 
 class water_check(threading.Thread):
 	def run(self):
@@ -39,3 +41,26 @@ class water_check(threading.Thread):
 				time.sleep(t_s)
 
 water_check().start()
+
+class motion_check(threading.Thread):
+    def run(self):
+        while True:
+            if(os.path.exists(motion_id)):
+                proc = Popen(['''/home/pi/Desktop/bot/motion_read.py'''], shell=True, stdout=PIPE, stderr=PIPE)
+                proc.wait()
+                m = proc.communicate()[0]
+                m = m.decode(encoding='utf-8')
+                m = m.replace('\n','')
+                if(m == u'обнаружено движение'):
+                        print m
+                        text = "Кто-то ходит!"
+                        subprocess.call(['''/home/pi/Desktop/bot/telegram_sender.py "%s"''' %text], shell=True)
+                        time.sleep(t_s3)
+                else:
+                        print("ERR")
+                        time.sleep(t_s)
+            else:
+                print('сигналка движения выключена')
+                time.sleep(t_s)
+
+motion_check().start()
