5c28a2e4c01ed390ecd59c19af047d1c9eafcb09
KasperZ <40818603+zaytseffffffff@users.noreply.github.com>
2021-12-04 14:04:20 +0300

    Change lib for dht11 sensor control

--- dht11_hum.py
+++ dht11_hum.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
-#project: poly-smart-home.ru
-import smbus
-import time
+#project: poly-smart-home.
 
-bus = smbus.SMBus(1)
-bus.write_byte(0x40, 0xF5)
-time.sleep(0.3)
-data0 = bus.read_byte(0x40)
-data1 = bus.read_byte(0x40)
-humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
-time.sleep(0.3)
-bus.write_byte(0x40, 0xF3)
-time.sleep(0.3)
-data0 = bus.read_byte(0x40)
-data1 = bus.read_byte(0x40)
-cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
-print "%.2f" %humidity
+import RPi.GPIO as GPIO
+import dht11
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 16
+instance = dht11.DHT11(pin = 16)
+result = instance.read()
+
+if result.is_valid():
+    print("%-3.1f" % result.humidity)
+else:
+    print("Error: %d" % result.error_code)
